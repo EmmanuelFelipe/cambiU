cd5d36365c4d464dce487becbba1bf021afc3a49
Emmanuel Felipe <oemmanuelf@gmail.com>
2023-05-03 20:31:14 -0300

    init

---
+++ src/main/java/org/unibh/App.java
@@ -0,0 +1,13 @@
+package org.unibh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
