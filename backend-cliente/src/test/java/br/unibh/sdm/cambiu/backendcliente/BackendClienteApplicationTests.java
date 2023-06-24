72cf870f8d62aa52a0b7cb57da87bd80c3c419b0
Emmanuel Felipe <oemmanuelf@gmail.com>
2023-06-24 19:06:21 -0300

    create backend-cliente

---
+++ backend-cliente/src/test/java/br/unibh/sdm/cambiu/backendcliente/BackendClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unibh.sdm.cambiu.backendcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
